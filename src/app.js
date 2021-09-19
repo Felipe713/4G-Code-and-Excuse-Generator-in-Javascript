0bd61dbc75f75b6780a98babf9be8e7ed0ed6ca5
Felipe713 <felipeivp@gmail.com>
2021-09-19 17:29:19 +0000

    tarea terminada

--- src/app.js
+++ src/app.js
@@ -7,5 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  // console.log("Hello Rigo from the console!");
+  let who = ["El perro", "Mi abuela", "Su tortuga", "Mi pajaro"];
+  let action = ["comio", "orino", "aplasto", "rompio"];
+  let what = ["mi tarea", "las llaves", "el auto"];
+  let when = [
+    "antes de la clase",
+    "justo a tiempo",
+    "cuando termino",
+    "durante mi almuerzo",
+    "mientras rezaba"
+  ];
+  var aleatorio4 = Math.floor(Math.random() * 4);
+  var aleatorio3 = Math.floor(Math.random() * 3);
+  var aleatorio5 = Math.floor(Math.random() * 5);
+  var excusa =
+    who[aleatorio4] +
+    " " +
+    action[aleatorio4] +
+    " " +
+    what[aleatorio3] +
+    " " +
+    when[aleatorio5];
+  let parrafo = this.document.getElementById("excuse");
+  parrafo.innerHTML = excusa;
 };
